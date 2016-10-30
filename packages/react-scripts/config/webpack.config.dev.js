dfb2b27f3a30f2bb924a16e31bee446b8ab73975
Tal <tshani@gmail.com>
2016-10-30 16:49:54 +0200

    Adding relay babel plugin to react-scripts

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -123,7 +123,8 @@ module.exports = {
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
-          cacheDirectory: true
+          cacheDirectory: true,
+          plugins: ['react-relay']
         }
       },
       // "postcss" loader applies autoprefixer to our CSS.
